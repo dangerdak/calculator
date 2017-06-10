a7cb073e8577ee67a6b2c606ad0fb7dd7d8e49dc
Dakshina Scott <dakshina.scott@gmail.com>
2017-06-10 16:17:31 +0100

    Adds handlers

--- main.js
+++ main.js
@@ -11,9 +11,24 @@ window.onload = function() {
 
   var calculationElt = document.getElementById('js-calculation');
   resultElt.textContent  = result;
+
+  var buttons = document.getElementsByClassName('btn');
+  for (var i = 0; buttons[i]; i++) {
+    buttons[i].addEventListener('click', function(e) {
+      calculation.string += e.target.textContent;
+      calculationElt.textContent = calculation.string;
+      if (calculation.run) {
+        calculation.run = handlers[e.target.id].bind(calculation.run);
+      }
+      else {
+        calculation.run = handlers[e.target.id];
+      }
+      
+    });
+  }
 };
 
-var op = {
+var handlers = {
   plus: function(rightOperand) {
     return function(leftOperand) {
       return leftOperand + rightOperand;
@@ -34,14 +49,16 @@ var op = {
       return leftOperand / rightOperand;
     };
   },
+  zero: function(fn) { return fn ? fn(0) : 0; },
+  one: function(fn) { return fn ? fn(1) : 1; },
+  two: function(fn) { return fn ? fn(2) : 2; },
+  three: function(fn) { return fn ? fn(3) : 3; },
+  four: function(fn) { return fn ? fn(4) : 4; },
+  five: function(fn) { return fn ? fn(5) : 5; },
+  six: function(fn) { return fn ? fn(6) : 6; },
+  seven: function(fn) { return fn ? fn(7) : 7; },
+  eight: function(fn) { return fn ? fn(8) : 8; },
+  nine: function(fn) { return fn ? fn(9) : 9; }
 };
-function zero(fn) { return fn ? fn(0) : 0; }
-function one(fn) { return fn ? fn(1) : 1; }
-function two(fn) { return fn ? fn(2) : 2; }
-function three(fn) { return fn ? fn(3) : 3; }
-function four(fn) { return fn ? fn(4) : 4; }
-function five(fn) { return fn ? fn(5) : 5; }
-function six(fn) { return fn ? fn(6) : 6; }
-function seven(fn) { return fn ? fn(7) : 7; }
-function eight(fn) { return fn ? fn(8) : 8; }
-function nine(fn) { return fn ? fn(9) : 9; }
+
+
